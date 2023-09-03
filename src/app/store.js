73d1d48755531a5fdc9bc4afc157ed886cbe358a
rafikaauda <rafika.auda4@gmail.com>
2023-09-04 00:36:43 +0700

    add config store crud todo

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from '../feature/authSlice';
+import authSlice from '../feature/auth/authSlice';
+import todoSlice from '../feature/todo/todoSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    todo: todoSlice,
   },
 });
---
+++ src/feature/todo/todoSlice.js
@@ -0,0 +1,90 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// read all todo
+export const getTodos = createAsyncThunk('todo', async () => {
+  const response = await axios.get(
+    'https://mockup-binar-be-production.up.railway.app/todo',
+  );
+  return response.data.payload;
+});
+
+// read todo based user id
+export const getTodoById = createAsyncThunk('todo/:userId', async () => {
+  const UserId = localStorage.getItem('userId');
+  const response = await axios.get(
+    `https://mockup-binar-be-production.up.railway.app/todo/${UserId}`,
+  );
+  return response.data.data;
+});
+
+// create todo baser user id
+export const addTodo = createAsyncThunk(
+  'todo/add',
+  async ({ todo, keterangan, user_id }) => {
+    const response = await axios.post(
+      `https://mockup-binar-be-production.up.railway.app/todo/add`,
+      { todo, keterangan, user_id },
+    );
+    return response.data;
+  },
+);
+
+export const updateTodo = createAsyncThunk(
+  '/update/:userid/:id',
+  async ({ id, todo, keterangan, user_id }) => {
+    const UserId = localStorage.getItem('userId');
+    const response = await axios.put(
+      `https://mockup-binar-be-production.up.railway.app/todo/${UserId}/${id}`,
+      { todo, keterangan, user_id },
+    );
+    return response.data;
+  },
+);
+
+// delete todo baser user id and id todo
+export const deleteTodo = createAsyncThunk('todo/:userid/:id', async (id) => {
+  const UserId = localStorage.getItem('userId');
+  await axios.delete(
+    `https://mockup-binar-be-production.up.railway.app/todo/${UserId}/${id}`,
+  );
+  return id;
+});
+
+const todoEntity = createEntityAdapter({
+  selectId: (diamond) => diamond.id,
+});
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: todoEntity.getInitialState(),
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTodos.fulfilled, (state, action) => {
+        todoEntity.setAll(state, action.payload);
+      })
+      .addCase(getTodoById.fulfilled, (state, action) => {
+        todoEntity.setAll(state, action.payload);
+      })
+      .addCase(addTodo.fulfilled, (state, action) => {
+        todoEntity.addOne(state, action.payload);
+      })
+      .addCase(deleteTodo.fulfilled, (state, action) => {
+        todoEntity.removeOne(state, action.payload);
+      })
+      .addCase(updateTodo.fulfilled, (state, action) => {
+        todoEntity.updateOne(state, {
+          id: action.payload.id,
+          updates: action.payload,
+        });
+      });
+  },
+});
+
+export const todoSelector = todoEntity.getSelectors((state) => state.todo);
+export default todoSlice.reducer;
