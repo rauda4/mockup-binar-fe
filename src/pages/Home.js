52a4ced0c455b24d66637e405d879bc004d7e4d4
rafikaauda <rafika.auda4@gmail.com>
2023-09-04 00:37:12 +0700

    add delete handler todo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import FormAddTodo from '../compnents/FormAddTodo';
+import {
+  getTodoById,
+  todoSelector,
+  deleteTodo,
+} from '../feature/todo/todoSlice';
 
 export default function Home() {
-  const username = localStorage.getItem('username');
+  // const username = localStorage.getItem('username');
+  const userid = localStorage.getItem('userId');
+  const dispatch = useDispatch();
+  const todos = useSelector(todoSelector.selectAll);
+
+  useEffect(() => {
+    dispatch(getTodoById());
+  }, [dispatch]);
+
   return (
     <>
       <div className='bg-neutral-900 py-9'></div>
@@ -9,21 +25,41 @@ export default function Home() {
         {/* content */}
         <div className=''>
           <div className='flex justify-center gap-10'>
+            <FormAddTodo />
             <section>
               <div className='bg-neutral-900 rounded-xl'>
-                <div className='w-96 py-10 px-10'>
+                <div className='w-full py-10 px-10'>
                   <h1 className='text-white'>
-                    <h1 className='flex justify-center'>
-                      Hello, {username} !!
-                    </h1>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum
+                    <table className='text-left table-auto border-separate border-spacing-x-5'>
+                      <thead>
+                        <tr>
+                          <th>No</th>
+                          <th>Todo List</th>
+                          <th>Keterangan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {todos.map((item, index) => {
+                          return (
+                            <tr key={item.id}>
+                              <td>{index + 1}</td>
+                              <td>{item.todo}</td>
+                              <td>{item.keterangan}</td>
+                              <td>
+                                <button
+                                  onClick={() => dispatch(deleteTodo(item.id))}
+                                  className='bg-red-600 mx-2 hover:bg-red-900 text-white py-1 px-4 rounded'>
+                                  Delete
+                                </button>
+                                <button className='bg-yellow-600 hover:bg-yellow-900 text-white py-1 px-4 rounded'>
+                                  <Link to={`/${userid}/${item.id}`}>Edit</Link>
+                                </button>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
                   </h1>
                 </div>
               </div>
