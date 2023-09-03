273095a700eba81e0ffb5075c97bb00a810c9c8d
rafikaauda <rafika.auda4@gmail.com>
2023-09-03 14:31:07 +0700

    add footer

--- src/layout/LandingPage.js
+++ src/layout/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Navbar from '../compnents/Navbar';
+import Footer from '../compnents/Footer';
 
 export default function LandingPage() {
   return (
@@ -17,6 +18,7 @@ export default function LandingPage() {
           element={<Navigate to={'/'} />}
         />
       </Routes>
+      <Footer />
     </>
   );
 }
