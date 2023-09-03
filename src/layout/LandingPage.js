061ade3cbf3e0420192d26bb148c6d54bd5435d5
rafikaauda <rafika.auda4@gmail.com>
2023-09-04 00:37:30 +0700

    add route for edit page

--- src/layout/LandingPage.js
+++ src/layout/LandingPage.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Navbar from '../compnents/Navbar';
 import Footer from '../compnents/Footer';
+import ToogleEdit from '../compnents/ToogleEdit';
 
 export default function LandingPage() {
   return (
@@ -12,6 +13,10 @@ export default function LandingPage() {
         <Route
           path='/'
           element={<Home />}
+        />
+        <Route
+          path='/:userid/:id'
+          element={<ToogleEdit />}
         />
         <Route
           path='/*'
