4d9c955f0a488ff5a2ad8d2f80c935cc93055fd2
rafikaauda <rafika.auda4@gmail.com>
2023-09-03 12:58:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Setelah membuat file BE, selanjutnya membuat tampilan di sisi front end 
+
+1. membuat config tailwind
+2. membuat ui register
+3. membuat ui login
+4. membuat ui home 
+5. membuat config store redux
+6. membuat auth slice login
+7. membuat auth slice register
+8. membuat auth slice logout
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './layout/LandingPage';
+import AuthPage from './layout/AuthPage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path='/*'
+          element={<LandingPage />}
+        />
+        <Route
+          path='/auth/*'
+          element={<AuthPage />}
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
